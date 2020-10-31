2f0b17e526def4b4e831aa8fa873b3138e8c6974
Noah Salvi <noah.salvi@ti8m.ch>
2020-10-31 13:57:39 +0100

    add dummy function

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import favicon from "serve-favicon";
+import { User } from "../models/user";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(express.static("/public"));
+
+app.get("/", (req, res) => {
+  console.log("test");
+  res.send({ text: "Hello World!" });
+});
+
+app.get("/noah", (req, res) => {
+  const noah: User = { firstName: "Noah", name: "Salvi" };
+  res.send(noah);
+});
+
+app.listen(PORT, () => console.log("App is listening on port " + PORT));
